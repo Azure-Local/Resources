5586beef4f003e415b193a04bd1da13e543740e7
fsegurai <fabiansegu@gmail.com>
2023-02-06 15:11:43 -0600

    fix/deployment script

--- Installation/install-vscode.ps1
+++ Installation/install-vscode.ps1
@@ -1,3 +1,6 @@
 $url = "https://update.code.visualstudio.com/latest/win32-x64-user/stable"
-$output = "C:\temp\vscode-setup.exe"
+mkdir "C:\Program Files\automatedDownloads\vscode" 
+$output = "C:\Program Files\automatedDownloads\vscode\vscode-install.exe"
 Invoke-WebRequest -Uri $url -OutFile $output
+
+Start-Process -FilePath "C:\Program Files\automatedDownloads\vscode\vscode-install.exe" -Wait -ArgumentList "/S"
